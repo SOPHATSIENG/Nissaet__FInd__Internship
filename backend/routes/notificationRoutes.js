9c7fdd8e3a5bd421c1f4254da558e63810c29e09
CodexSandboxOffline <youphat45@gmail.com>
2026-03-20 15:30:22 +0700

    Fix notification routes auth middleware

--- backend/routes/notificationRoutes.js
+++ backend/routes/notificationRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const notificationController = require('../controllers/notificationController');
-const { protect } = require('../middleware/auth');
+const { authenticate } = require('../middleware/auth');
 
 // All notification routes are protected
-router.use(protect);
+router.use(authenticate);
 
 router.get('/', notificationController.getNotifications);
 router.put('/mark-all-read', notificationController.markAllAsRead);
